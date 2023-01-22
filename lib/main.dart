f25930ea120577780c644f6a0ca2d1c9350f5843
Grigory Latyshev <thegriglat@gmail.com>
2023-01-22 19:15:47 +0300

    format

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 
 class ImageResponse {
   final String file;
+
   const ImageResponse({
     required this.file,
   });
@@ -21,7 +22,7 @@ Future<ImageResponse> fetchImage() async {
   final response = await http.get(Uri.parse('https://aws.random.cat/meow'));
   if (response.statusCode == 200) {
     return ImageResponse.fromJson(jsonDecode(response.body));
-  }  else {
+  } else {
     throw Exception("Failed to load cat");
   }
 }
@@ -44,53 +45,57 @@ class _MyAppState extends State<MyApp> {
     var catImage = fetchImage();
     return MaterialApp(
         title: 'Random cat',
-        theme:flutterNesTheme(),
-        home:Scaffold(
-          appBar: AppBar(
-            title: const Text('Cat explorer'),
-          ),
-          body: Column(
-          children: [ NesContainer(child: FutureBuilder<ImageResponse>(
-              future: catImage,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Image.network(
-                    snapshot.data!.file,
-                    fit: BoxFit.fill,
-                    loadingBuilder: (BuildContext context, Widget child,
-                        ImageChunkEvent? loadingProgress) {
-                      if (loadingProgress == null) return child;
-                      return Center(
-                        child: CircularProgressIndicator(
-                          value: loadingProgress.expectedTotalBytes != null
-                              ? loadingProgress.cumulativeBytesLoaded /
-                              loadingProgress.expectedTotalBytes!
-                              : null,
-                        ),
-                      );
-                    },
-                  );
-                } else if (snapshot.hasError) {
-                  return Text('${snapshot.error}');
-                }
-                // By default, show a loading spinner.
-                return const CircularProgressIndicator();
-              }
-          )),
-        Divider()
-      ])
-         ,floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-            floatingActionButton:  NesButton(
-    type: NesButtonType.normal,
-        onPressed: () => setState(() {
-      catImage = fetchImage();
-    }),
-    child: const Text("I want more!"),
-    )
-        )
-
-
-    );
+        theme: flutterNesTheme(),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Cat explorer'),
+            ),
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                      child: NesContainer(
+                          child: FutureBuilder<ImageResponse>(
+                              future: catImage,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  return Image.network(
+                                    snapshot.data!.file,
+                                    fit: BoxFit.fill,
+                                    loadingBuilder: (BuildContext context,
+                                        Widget child,
+                                        ImageChunkEvent? loadingProgress) {
+                                      if (loadingProgress == null) return child;
+                                      return Center(
+                                        child: CircularProgressIndicator(
+                                          value: loadingProgress
+                                                      .expectedTotalBytes !=
+                                                  null
+                                              ? loadingProgress
+                                                      .cumulativeBytesLoaded /
+                                                  loadingProgress
+                                                      .expectedTotalBytes!
+                                              : null,
+                                        ),
+                                      );
+                                    },
+                                  );
+                                } else if (snapshot.hasError) {
+                                  return Text('${snapshot.error}');
+                                }
+                                // By default, show a loading spinner.
+                                return const CircularProgressIndicator();
+                              })))
+                ]),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: NesButton(
+              type: NesButtonType.normal,
+              onPressed: () => setState(() {
+                catImage = fetchImage();
+              }),
+              child: const Text("I want more!"),
+            )));
   }
-
 }
